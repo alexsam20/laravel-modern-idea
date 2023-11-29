1f8623602bf8739b43891cab1a3fc07a1fa39d14
AlexSaM <alsam3637@gmail.com>
2023-11-29 16:04:05 -0500

    Form Submission.

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', [\App\Http\Controllers\DashboardController::class, 'index']);
+Route::get('/', [\App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
+
+Route::post('/idea', [\App\Http\Controllers\IdeaController::class, 'store'])->name('idea.create');
 
 Route::get('/terms', function (){
     return view('terms');

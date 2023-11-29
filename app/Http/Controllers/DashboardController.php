f8bb55fe0b8aba3662776dc88303dbdbd035c821
AlexSaM <alsam3637@gmail.com>
2023-11-29 14:50:58 -0500

    Creating Models Idea.

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,19 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Idea;
 use Illuminate\Http\Request;
-use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        $users = [
-            ['name' => 'Alex', 'age' => 53],
-            ['name' => 'Dan', 'age' => 25],
-            ['name' => 'Bob', 'age' => 17],
-        ];
+//        $idea = new Idea([
+//            'content' => 'test',
+//        ]);
+//        $idea->save();
 
-        return view('dashboard', compact('users'));
+        return view('dashboard', [
+            'ideas' => Idea::orderBy('created_at', 'DESC')->get(),
+        ]);
     }
 }

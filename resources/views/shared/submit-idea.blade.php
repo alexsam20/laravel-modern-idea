ca2ae9df005b2bce8e68611e290f1bda65031a4c
AlexSaM <alsam3637@gmail.com>
2023-11-30 09:49:02 -0500

    Update.

--- resources/views/shared/submit-idea.blade.php
+++ resources/views/shared/submit-idea.blade.php
@@ -3,7 +3,7 @@
     <form action="{{ route('ideas.create') }}" method="post">
         @csrf
         <div class="mb-3">
-            <textarea name="idea" class="form-control" id="idea" rows="3"></textarea>
+            <textarea name="content" class="form-control" id="content" rows="3"></textarea>
             @error('idea')
                 <span class="d-block fs-6 text-danger mt-2"> {{ $message }} </span>
             @enderror
--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,8 @@ Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
 Route::get('/ideas/{idea}', [IdeaController::class, 'show'])->name('ideas.show');
 Route::post('/ideas', [IdeaController::class, 'store'])->name('ideas.create');
+Route::get('/ideas/{idea}/edit', [IdeaController::class, 'edit'])->name('ideas.edit');
+Route::put('/ideas/{idea}', [IdeaController::class, 'update'])->name('ideas.update');
 Route::delete('/ideas/{idea}', [IdeaController::class, 'destroy'])->name('ideas.destroy');
 
 Route::get('/terms', function (){

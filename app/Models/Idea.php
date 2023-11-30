ad12869f5dcae90c42279115cc721ca07e053335
AlexSaM <alsam3637@gmail.com>
2023-11-30 16:45:39 -0500

    $fillable and $guarded.

--- app/Models/Idea.php
+++ app/Models/Idea.php
@@ -9,6 +9,14 @@ class Idea extends Model
 {
     use HasFactory;
 
+//    protected $guarded = [
+//        'id',
+//        'created_at',
+//        'updated_at',
+//    ];
+
+//    protected $guarded = [];
+
     /**
      * The attributes that are mass assignable.
      *

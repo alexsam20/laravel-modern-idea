cb781e7212941c649fb09c1944fbf0fbd1211f3f
AlexSaM <alsam3637@gmail.com>
2023-11-30 13:21:43 -0500

    Creating Comments. Relationship.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\IdeaController;
 use Illuminate\Support\Facades\Route;
@@ -12,6 +13,8 @@ Route::post('/ideas', [IdeaController::class, 'store'])->name('ideas.create');
 Route::get('/ideas/{idea}/edit', [IdeaController::class, 'edit'])->name('ideas.edit');
 Route::put('/ideas/{idea}', [IdeaController::class, 'update'])->name('ideas.update');
 Route::delete('/ideas/{idea}', [IdeaController::class, 'destroy'])->name('ideas.destroy');
+
+Route::post('/ideas/{idea}/comments', [CommentController::class, 'store'])->name('ideas.comments.store');
 
 Route::get('/terms', function (){
     return view('terms');

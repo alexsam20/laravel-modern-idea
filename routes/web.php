58c701844187056c8fc763d18ccf81e957566a9e
AlexSaM <alsam3637@gmail.com>
2023-11-30 22:56:13 -0500

    Route Grouping.

--- routes/web.php
+++ routes/web.php
@@ -6,22 +6,25 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\IdeaController;
 use Illuminate\Support\Facades\Route;
 
-
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
-Route::get('/ideas/{idea}', [IdeaController::class, 'show'])->name('ideas.show');
-Route::post('/ideas', [IdeaController::class, 'store'])->name('ideas.create');
-Route::get('/ideas/{idea}/edit', [IdeaController::class, 'edit'])->name('ideas.edit')->middleware('auth');
-Route::put('/ideas/{idea}', [IdeaController::class, 'update'])->name('ideas.update')->middleware('auth');
-Route::delete('/ideas/{idea}', [IdeaController::class, 'destroy'])->name('ideas.destroy')->middleware('auth');
+Route::group(['prefix' => 'ideas/', 'as' => 'ideas.'], function () {
 
-Route::post('/ideas/{idea}/comments', [CommentController::class, 'store'])->name('ideas.comments.store')->middleware('auth');
+    Route::post('', [IdeaController::class, 'store'])->name('store');
 
-Route::get('/register', [AuthController::class, 'register'])->name('register');
-Route::post('/register', [AuthController::class, 'store']);
-Route::get('/login', [AuthController::class, 'login'])->name('login');
-Route::post('/login', [AuthController::class, 'authenticate']);
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::get('/{idea}', [IdeaController::class, 'show'])->name('show');
+
+    Route::group(['middleware' => ['auth']], function () {
+
+        Route::get('/{idea}/edit', [IdeaController::class, 'edit'])->name('edit');
+
+        Route::put('/{idea}', [IdeaController::class, 'update'])->name('update');
+
+        Route::delete('/{idea}', [IdeaController::class, 'destroy'])->name('destroy');
+
+        Route::post('/{idea}/comments', [CommentController::class, 'store'])->name('comments.store');
+    });
+});
 
 Route::get('/terms', function (){
     return view('terms');
